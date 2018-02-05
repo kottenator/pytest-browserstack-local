b03a45f494e810c79b45979f1ea8670f78f37233
Rostyslav Bryzgunov <kottenator@gmail.com>
2018-02-04 21:46:00 -0500

    Support for `pytest-variables` and `BROWSERSTACK_ACCESS_KEY` env var.

--- src/pytest_browserstack_local/utils.py
+++ src/pytest_browserstack_local/utils.py
@@ -1,4 +1,8 @@
+import os
 import re
+
+
+BROWSERSTACK_ACCESS_KEY_ENV_VAR = 'BROWSERSTACK_ACCESS_KEY'
 
 
 def convert_camelcase(s):
@@ -52,13 +56,33 @@ def parse_config(config):
     """
     browserstack_local_path = config.getoption('browserstack_local_path')
     browserstack_local_cmd = [browserstack_local_path]
+    browserstack_local_config = {}
 
+    # Integration with ``pytest-variables``
+    for key, value in config._variables.get('BrowserStackLocal', {}).items():
+        # ``False`` value is skipped as there was no such argument.
+        if value is False:
+            continue
+
+        # Both ``True`` and ``None`` values are used for "flag" arguments (i.e. no value).
+        if value is True:
+            value = None
+
+        browserstack_local_config[convert_to_arg(key)] = value
+
+    if BROWSERSTACK_ACCESS_KEY_ENV_VAR in os.environ:
+        browserstack_local_config.pop('-k', None)
+        browserstack_local_config['--key'] = os.getenv(BROWSERSTACK_ACCESS_KEY_ENV_VAR)
+
+    # CLI arguments override ``pytest-variables``.
     for kv in config.getoption('browserstack_local_arguments'):
-        if '=' in kv:
-            key, value = kv.split('=', 1)
-            browserstack_local_cmd.append(convert_to_arg(key))
-            browserstack_local_cmd.append(value)
-        else:
-            browserstack_local_cmd.append(convert_to_arg(kv))
+        key, value = kv.split('=', 1) if '=' in kv else (kv, None)
+        browserstack_local_config[convert_to_arg(key)] = value
+
+    for key, value in browserstack_local_config.items():
+        browserstack_local_cmd.append(key)
+
+        if value is not None:
+            browserstack_local_cmd.append(str(value))
 
     return browserstack_local_cmd
