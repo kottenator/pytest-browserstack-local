9f2d7f857c40596c633b9035bff6b15a787bc1e4
Rostyslav Bryzgunov <kottenator@gmail.com>
2018-02-04 21:44:55 -0500

    Remove unnecessary complexity from tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,8 @@
 import os
 
 
-def pytest_addoption(parser):
-    group = parser.getgroup('browserstack-local-test')
-    group.addoption(
-        '--browserstack-local-real',
-        action='store_true',
-        help='Run real BrowserStackLocal instead of fake one in tests.'
+def pytest_configure():
+    os.environ['PATH'] = os.pathsep.join(
+        [os.path.join(os.path.dirname(__file__), 'fakes')] +
+        os.getenv('PATH', '').split(os.pathsep)
     )
-
-
-def pytest_configure(config):
-    if not config.getoption('browserstack_local_real'):
-        os.environ['PATH'] = os.pathsep.join(
-            [os.path.join(os.path.dirname(__file__), 'fakes')] +
-            os.getenv('PATH', '').split(os.pathsep)
-        )
