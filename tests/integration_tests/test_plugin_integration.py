a31084a003037b40282102867e5e71dc02cf0918
Rostyslav Bryzgunov <kottenator@gmail.com>
2018-02-08 22:32:40 -0500

    Simple integration tests added.

--- tests/integration_tests/test_plugin_integration.py
+++ tests/integration_tests/test_plugin_integration.py
@@ -1,14 +1,20 @@
 import os
+import shutil
+
 import pytest
+
+
+TEST_DIR = os.path.dirname(os.path.dirname(__file__))
+RUN_DIR = os.path.join(TEST_DIR, 'run')
+TEST_ACCESS_KEY = '123'
+WRONG_ACCESS_KEY = '111'
 
 
 @pytest.fixture(autouse=True)
 def clean_fake_locks():
-    for lock_name in ['default', '789']:
-        lock_file = 'browserstack-local-identifier-{}.lock'.format(lock_name)
-
-        if os.path.exists(lock_file):
-            os.remove(lock_file)
+    if os.path.exists(RUN_DIR):
+        shutil.rmtree(RUN_DIR)
+    os.mkdir(RUN_DIR)
 
 
 @pytest.mark.sensitive
@@ -21,42 +27,87 @@ def test_remote(httpserver, selenium):
     assert heading.text == "Hello, World!"
 
 
-def test_plugin_process_run(testdir):
+def test_plugin_process_ok(testdir):
     """
     Test that ``BrowserStackLocal`` subprocess is created and has a PID.
 
     :param testdir: ``pytester`` fixture.
     """
     testdir.makepyfile("""
-        def test_plugin_process_run(browserstack_local):
+        def test_process(browserstack_local):
             assert not browserstack_local['daemon']
             assert browserstack_local['process']
             assert browserstack_local['process'].pid
-            assert browserstack_local['cmd'] == ['BrowserStackLocal', '--key', '123']
-    """)
+            assert browserstack_local['cmd'] == ['BrowserStackLocal', '--key', '{}']
+    """.format(TEST_ACCESS_KEY))
 
-    result = testdir.runpytest('--browserstack-local', '--browserstack-local-argument', 'key=123')
+    result = testdir.runpytest(
+        '--browserstack-local', '--browserstack-local-argument', 'key={}'.format(TEST_ACCESS_KEY)
+    )
     result.assert_outcomes(passed=1)
 
 
-def test_plugin_daemon_run(testdir):
+def test_plugin_process_fail(testdir):
+    """
+    Test that ``BrowserStackLocal`` subprocess is not created when there's wrong access key.
+
+    :param testdir: ``pytester`` fixture.
+    """
+    testdir.makepyfile("""
+        def test_process(browserstack_local):
+            assert browserstack_local == {{
+                'daemon': None,
+                'process': None,
+                'cmd': ['BrowserStackLocal', '--key', '{}']
+            }}
+    """.format(WRONG_ACCESS_KEY))
+
+    result = testdir.runpytest(
+        '--browserstack-local', '--browserstack-local-argument', 'key={}'.format(WRONG_ACCESS_KEY)
+    )
+    result.assert_outcomes(passed=1)
+
+
+def test_plugin_daemon_ok(testdir):
     """
     Test that ``BrowserStackLocal`` daemon is invoked and has a PID.
 
     :param testdir: ``pytester`` fixture.
     """
     testdir.makepyfile("""
-        def test_plugin_daemon_run(browserstack_local):
+        def test_daemon(browserstack_local):
             assert not browserstack_local['process']
             assert browserstack_local['daemon']
             assert browserstack_local['daemon']['pid']
             assert browserstack_local['cmd'] == [
-                'BrowserStackLocal', '--key', '123', '--daemon', 'start'
+                'BrowserStackLocal', '--key', '{}', '--daemon', 'start'
             ]
-    """)
+    """.format(TEST_ACCESS_KEY))
 
     result = testdir.runpytest(
-        '--browserstack-local', '--browserstack-local-argument', 'key=123',
+        '--browserstack-local', '--browserstack-local-argument', 'key={}'.format(TEST_ACCESS_KEY),
+        '--browserstack-local-argument', 'daemon=start'
+    )
+    result.assert_outcomes(passed=1)
+
+
+def test_plugin_daemon_fail(testdir):
+    """
+    Test that ``BrowserStackLocal`` daemon is not invoked when there's wrong access key.
+
+    :param testdir: ``pytester`` fixture.
+    """
+    testdir.makepyfile("""
+        def test_daemon(browserstack_local):
+            assert browserstack_local == {{
+                'daemon': None,
+                'process': None,
+                'cmd': ['BrowserStackLocal', '--key', '{}', '--daemon', 'start']
+            }}
+    """.format(WRONG_ACCESS_KEY))
+
+    result = testdir.runpytest(
+        '--browserstack-local', '--browserstack-local-argument', 'key={}'.format(WRONG_ACCESS_KEY),
         '--browserstack-local-argument', 'daemon=start'
     )
     result.assert_outcomes(passed=1)
