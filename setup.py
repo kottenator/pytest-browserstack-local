4e406b84beb91b93384e8ec720d7e1a23b577ce6
Rostyslav Bryzgunov <kottenator@gmail.com>
2018-02-09 01:11:21 -0500

    First release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pytest-browserstack-local',
-    version='0.5.0a1',
+    version='0.5.0',
     description='``py.test`` plugin to run ``BrowserStackLocal`` in background.',
     long_description=(
         'May be useful for Continuous Integration. '
