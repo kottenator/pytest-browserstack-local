dba3349d98d3477207087f44b7160dcb80be7c3f
Rostyslav Bryzgunov <kottenator@gmail.com>
2018-02-08 22:59:41 -0500

    More integration tests added.

--- tests/integration_tests/test_plugin_integration.py
+++ tests/integration_tests/test_plugin_integration.py
@@ -1,8 +1,10 @@
+import json
 import os
 import shutil
 
 import pytest
 
+from pytest_browserstack_local.utils import BROWSERSTACK_ACCESS_KEY_ENV_VAR
 
 TEST_DIR = os.path.dirname(os.path.dirname(__file__))
 RUN_DIR = os.path.join(TEST_DIR, 'run')
@@ -108,6 +110,103 @@ def test_plugin_daemon_fail(testdir):
 
     result = testdir.runpytest(
         '--browserstack-local', '--browserstack-local-argument', 'key={}'.format(WRONG_ACCESS_KEY),
+        '--browserstack-local-argument', 'daemon=start'
+    )
+    result.assert_outcomes(passed=1)
+
+
+def test_plugin_conf_file(testdir):
+    """
+    Test that ``BrowserStackLocal`` reads parameters from a config file.
+
+    :param testdir: ``pytester`` fixture.
+    """
+    conf_file = os.path.join(RUN_DIR, 'browserstack-local-test.json')
+
+    with open(conf_file, 'w') as f:
+        json.dump({'BrowserStackLocal': {'key': TEST_ACCESS_KEY}}, f)
+
+    testdir.makepyfile("""
+        def test_daemon(browserstack_local):
+            assert browserstack_local['cmd'] == [
+                'BrowserStackLocal', '--key', '{}', '--daemon', 'start'
+            ]
+    """.format(TEST_ACCESS_KEY))
+
+    result = testdir.runpytest(
+        '--browserstack-local', '--variables', conf_file,
+        '--browserstack-local-argument', 'daemon=start'
+    )
+    result.assert_outcomes(passed=1)
+
+
+def test_plugin_env_var(testdir, monkeypatch):
+    """
+    Test that ``BrowserStackLocal`` reads parameters (e.g. access key) passed as
+    environment variables.
+
+    :param testdir: ``pytester`` fixture.
+    """
+    monkeypatch.setenv(BROWSERSTACK_ACCESS_KEY_ENV_VAR, TEST_ACCESS_KEY)
+
+    testdir.makepyfile("""
+        def test_daemon(browserstack_local):
+            assert browserstack_local['cmd'] == [
+                'BrowserStackLocal', '--key', '{}', '--daemon', 'start'
+            ]
+    """.format(TEST_ACCESS_KEY))
+
+    result = testdir.runpytest(
+        '--browserstack-local', '--browserstack-local-argument', 'daemon=start'
+    )
+    result.assert_outcomes(passed=1)
+
+
+def test_plugin_env_var_vs_conf_file(testdir, monkeypatch):
+    """
+    Test that ``BrowserStackLocal`` parameter (e.g. access key) passed as an environment variable
+    has higher priority that the one from a config file.
+
+    :param testdir: ``pytester`` fixture.
+    """
+    monkeypatch.setenv(BROWSERSTACK_ACCESS_KEY_ENV_VAR, '555')
+    conf_file = os.path.join(RUN_DIR, 'browserstack-local-test.json')
+
+    with open(conf_file, 'w') as f:
+        json.dump({'BrowserStackLocal': {'key': '333'}}, f)
+
+    testdir.makepyfile("""
+            def test_daemon(browserstack_local):
+                assert browserstack_local['cmd'] == [
+                    'BrowserStackLocal', '--key', '555', '--daemon', 'start'
+                ]
+        """)
+
+    result = testdir.runpytest(
+        '--browserstack-local', '--variables', conf_file,
+        '--browserstack-local-argument', 'daemon=start'
+    )
+    result.assert_outcomes(passed=1)
+
+
+def test_plugin_env_var_vs_arg(testdir, monkeypatch):
+    """
+    Test that ``BrowserStackLocal`` parameter (e.g. access key) passed as an environment variable
+    has lower priority that the one from a command-line argument.
+
+    :param testdir: ``pytester`` fixture.
+    """
+    monkeypatch.setenv(BROWSERSTACK_ACCESS_KEY_ENV_VAR, '555')
+
+    testdir.makepyfile("""
+            def test_daemon(browserstack_local):
+                assert browserstack_local['cmd'] == [
+                    'BrowserStackLocal', '--key', '777', '--daemon', 'start'
+                ]
+        """)
+
+    result = testdir.runpytest(
+        '--browserstack-local', '--browserstack-local-argument', 'key=777',
         '--browserstack-local-argument', 'daemon=start'
     )
     result.assert_outcomes(passed=1)
